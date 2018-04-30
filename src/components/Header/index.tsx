e35dad2ac6f538cc59387a3fef5114ea7d88582c
Tyler Collins <tjscollins@gmail.com>
2018-04-30 10:48:13 -1000

    Fetch nav links from WP installation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react'
 import styled from 'styled-components'
 import { transparentize } from 'polished'
 import Link from 'gatsby-link'
+import axios from 'axios';
 
-import { heights, dimensions, colors } from '../styles/variables'
-import { onEvent } from '../styles/mixins'
-import Container from './Container'
+import { heights, dimensions, colors } from '../../styles/variables';
+import { onEvent } from '../../styles/mixins';
+import { Container, Row, Column } from '../Grid';
 
 const StyledHeader = styled.header`
   height: ${heights.header}px;
@@ -35,12 +36,52 @@ interface HeaderProps {
   title: string
 }
 
-const Header: React.SFC<HeaderProps> = ({ title }) => (
-  <StyledHeader>
-    <HeaderInner>
-      <HomepageLink to="/" href="/">{title}</HomepageLink>
-    </HeaderInner>
-  </StyledHeader>
-)
+class Header extends React.Component<HeaderProps> {
+
+    private domain = 'http://localhost:80';
+
+    constructor(props) {
+        super(props);
+        this.fetchHeaderContent = this.fetchHeaderContent.bind(this);
+        this.getPages = this.getPages.bind(this);
+    }
+
+    public componentDidMount() {
+        this.fetchHeaderContent();
+    }
+
+    public render () {
+        const { title } = this.props;
+        console.log('State', this.state);
+        return (
+            <StyledHeader>
+                <HeaderInner>
+                    <HomepageLink to="/" href="/">{title}</HomepageLink>
+                </HeaderInner>
+            </StyledHeader>
+        );
+    }
+
+    private async fetchHeaderContent() {
+        const navSections = await axios.get(`${this.domain}/wp-json/wp/v2/nav/`);
+
+        const headerLinks = await Promise.all(
+            navSections.data.map(this.getPages)
+        );
+
+        this.setState({headerLinks});
+    }
+
+    private async getPages(navSection) {
+        const pages = await axios.get(
+            `${this.domain}/wp-json/wp/v2/pages/?nav=${navSection.id}&status=publish&per_page=100`
+        );
+
+        return {
+            navSection,
+            pages
+        };
+    }
+}
 
 export default Header

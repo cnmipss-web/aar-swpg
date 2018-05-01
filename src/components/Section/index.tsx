8ce68990e3dbd124c9d250cada8e59284c4b07e2
Tyler Collins <tjscollins@gmail.com>
2018-05-01 09:08:12 -1000

    Add typings to Section component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react'
 const uuidv4 = require('uuid/v4');
 
-export default ({ data, section, children }) => (
+declare interface MarkdownNode {
+    frontmatter: {
+        title: string
+    }
+    html: string
+}
+
+declare interface MarkdownEdge {
+    node: MarkdownNode
+}
+
+declare interface SectionData {
+    allMarkdownRemark: {
+        edges: MarkdownEdge[]
+    }
+}
+
+declare interface SectionProps {
+    data: SectionData
+    section: string
+}
+
+const Section: React.SFC<SectionProps> = ({ data, section, children }) => (
     <section>
         <div dangerouslySetInnerHTML={{__html: getMarkDownContent(data, section) }} />
         {children}
     </section>
 );
 
-function getMarkDownContent(data, section) {
+export default Section;
+
+function getMarkDownContent(data: SectionData, section: string): string {
     return data.allMarkdownRemark.edges
         .filter(({node}) => node.frontmatter.title === section)
         .map(({node}) => node.html)[0]
